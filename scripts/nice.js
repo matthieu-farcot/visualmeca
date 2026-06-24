abed05d1310d821e8d412318977b2c0f82960bb4
Matthieu Farcot <511239+matthieu-farcot@users.noreply.github.com>
2026-06-24 11:16:01 +0200

    Add cycle-piston component with animation methods

--- scripts/nice.js
+++ scripts/nice.js
@@ -1,16 +1,131 @@
-const THREE = (typeof AFRAME !== 'undefined' && AFRAME.THREE) || window.THREE;
+AFRAME.registerComponent("cycle-piston", {
 
-// Composant pour l'environnement HDR
-AFRAME.registerComponent("hdr-env", {
-  init: function () {
-    const scene = this.el.sceneEl.object3D;
-    const loader = new THREE.TextureLoader();
-    loader.setPath("./objets/");
+    init() {
 
-    loader.load("cadre.jpg", (texture) => {
-      texture.mapping = THREE.EquirectangularReflectionMapping;
-//      scene.background = texture;
-      scene.environment = texture;
-    });
-  }
+        this.piston = this.el.querySelector("#piston");
+        this.mobile = this.el.querySelector("#pistonmobile");
+        this.pd = this.el.querySelector("#pistond");
+        this.pg = this.el.querySelector("#pistong");
+
+        // Positions d'origine
+        this.pistonPos = this.piston.object3D.position.clone();
+        this.mobilePos = this.mobile.object3D.position.clone();
+
+        // Rotations d'origine (en degrés)
+        this.pdRot = this.pd.getAttribute("rotation");
+        this.pgRot = this.pg.getAttribute("rotation");
+
+        this.bbAller();
+    },
+
+    //-----------------------------
+    // BB ALLER
+    //-----------------------------
+    bbAller() {
+
+        this.piston.setAttribute("animation__move", {
+            property: "position",
+            to: `${this.pistonPos.x} ${this.pistonPos.y-0.26} ${this.pistonPos.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pd.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pdRot.x} ${this.pdRot.y} ${this.pdRot.z-90}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pg.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pgRot.x} ${this.pgRot.y} ${this.pgRot.z+90}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.piston.addEventListener("animationcomplete__move",
+            () => this.bbRetour(),
+            {once:true});
+    },
+
+    //-----------------------------
+    // BB RETOUR
+    //-----------------------------
+    bbRetour() {
+
+        this.piston.setAttribute("animation__move", {
+            property: "position",
+            to: `${this.pistonPos.x} ${this.pistonPos.y} ${this.pistonPos.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pd.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pdRot.x} ${this.pdRot.y} ${this.pdRot.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pg.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pgRot.x} ${this.pgRot.y} ${this.pgRot.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.piston.addEventListener("animationcomplete__move",
+            () => this.ccAller(),
+            {once:true});
+    },
+
+    //-----------------------------
+    // CC ALLER
+    //-----------------------------
+    ccAller() {
+
+        this.mobile.setAttribute("animation__move", {
+            property: "position",
+            to: `${this.mobilePos.x+0.19} ${this.mobilePos.y} ${this.mobilePos.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pg.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pgRot.x} 180 90`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.mobile.addEventListener("animationcomplete__move",
+            () => this.ccRetour(),
+            {once:true});
+    },
+
+    //-----------------------------
+    // CC RETOUR
+    //-----------------------------
+    ccRetour() {
+
+        this.mobile.setAttribute("animation__move", {
+            property: "position",
+            to: `${this.mobilePos.x} ${this.mobilePos.y} ${this.mobilePos.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.pg.setAttribute("animation__rot", {
+            property: "rotation",
+            to: `${this.pgRot.x} ${this.pgRot.y} ${this.pgRot.z}`,
+            dur: 1000,
+            easing: "linear"
+        });
+
+        this.mobile.addEventListener("animationcomplete__move",
+            () => this.bbAller(),
+            {once:true});
+    }
+
 });
