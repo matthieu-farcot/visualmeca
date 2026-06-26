b70a95b15974e5294ddc2f37e5059871c81f74d9
Matthieu Farcot <511239+matthieu-farcot@users.noreply.github.com>
2026-06-26 08:38:20 +0200

    Refactor cycle-piston component for animation flow

--- scripts/nice.js
+++ scripts/nice.js
@@ -1,118 +1,72 @@
 AFRAME.registerComponent("cycle-piston", {
 
-    schema: {
-        vitesse: {default: 0.25}   // unités par seconde
-    },
-
     init() {
 
         this.piston = this.el.querySelector("#piston");
         this.mobile = this.el.querySelector("#pistonmobile");
-        this.pd = this.el.querySelector("#pistond");
-        this.pg = this.el.querySelector("#pistong");
 
-        // Références
-        this.y0 = this.piston.object3D.position.y;
-        this.x0 = this.mobile.object3D.position.x;
-
-        this.courseY = 0.249;
-        this.courseX = 0.19;
-
-        this.etat = 0;
+        // Démarrage de la première animation
+        this.bbDown();
 
     },
 
-    tick(time, dt) {
+    //-----------------------------
+    // BB DESCENTE
+    //-----------------------------
+    bbDown() {
 
-        const dy = this.schema.vitesse * dt / 1000;
-        const dx = this.schema.vitesse * dt / 1000;
+        this.piston.emit("bbdown");
 
-        switch(this.etat){
+        this.piston.addEventListener(
+            "animationcomplete__bbdown",
+            () => this.ccGo(),
+            { once: true }
+        );
 
-        //--------------------------------------------------
-        // 0 Descente
-        //--------------------------------------------------
+    },
 
-        case 0:
+    //-----------------------------
+    // CC ALLER
+    //-----------------------------
+    ccGo() {
 
-            this.piston.object3D.position.y -= dy;
+        this.mobile.emit("ccgo");
 
-            this.pd.object3D.rotation.z -= Math.PI/2 * dt/1000;
+        this.mobile.addEventListener(
+            "animationcomplete__ccgo",
+            () => this.ccBack(),
+            { once: true }
+        );
 
-            if(this.piston.object3D.position.y <= this.y0-this.courseY){
+    },
 
-                this.piston.object3D.position.y=this.y0-this.courseY;
-                this.pd.object3D.rotation.z=-Math.PI/2;
+    //-----------------------------
+    // CC RETOUR
+    //-----------------------------
+    ccBack() {
 
-                this.etat=1;
+        this.mobile.emit("ccback");
 
-            }
+        this.mobile.addEventListener(
+            "animationcomplete__ccback",
+            () => this.bbUp(),
+            { once: true }
+        );
 
-        break;
+    },
 
-        //--------------------------------------------------
-        // 1 Déplacement droite
-        //--------------------------------------------------
+    //-----------------------------
+    // BB REMONTÉE
+    //-----------------------------
+    bbUp() {
 
-        case 1:
+        this.piston.emit("bbup");
 
-            this.mobile.object3D.position.x += dx;
-
-            this.pg.object3D.rotation.y += Math.PI/2*dt/1000;
-
-            if(this.mobile.object3D.position.x>=this.x0+this.courseX){
-
-                this.mobile.object3D.position.x=this.x0+this.courseX;
-
-                this.etat=2;
-
-            }
-
-        break;
-
-        //--------------------------------------------------
-        // 2 Retour gauche
-        //--------------------------------------------------
-
-        case 2:
-
-            this.mobile.object3D.position.x -= dx;
-
-            this.pg.object3D.rotation.y -= Math.PI/2*dt/1000;
-
-            if(this.mobile.object3D.position.x<=this.x0){
-
-                this.mobile.object3D.position.x=this.x0;
-
-                this.etat=3;
-
-            }
-
-        break;
-
-        //--------------------------------------------------
-        // 3 Montée
-        //--------------------------------------------------
-
-        case 3:
-
-            this.piston.object3D.position.y += dy;
-
-            this.pd.object3D.rotation.z += Math.PI/2 * dt/1000;
-
-            if(this.piston.object3D.position.y>=this.y0){
-
-                this.piston.object3D.position.y=this.y0;
-
-                this.pd.object3D.rotation.z=0;
-
-                this.etat=0;
-
-            }
-
-        break;
-
-        }
+        this.piston.addEventListener(
+            "animationcomplete__bbup",
+            () => this.bbDown(),
+            { once: true }
+        );
 
     }
 
